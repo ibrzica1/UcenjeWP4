c96892eff4ebb8647164ad727d7bf9043f009ed8
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-06-05 19:48:50 +0200

    Csharp_ucenje

---
+++ Scharp/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+    var a = 10;
+    var b = 3;
+    Console.WriteLine(a != b);
+}
---
+++ Scharp/Ucenje/Ucenjecs/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information //
+
+Console.WriteLine(7);
+Console.WriteLine(true);
+Console.WriteLine('A');
+Console.WriteLine("Igor Brzica");
+
