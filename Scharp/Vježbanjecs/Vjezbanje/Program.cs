7d0e2030c6fbd7aa3f11a67d4a63e8c5aafeb104
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-06-14 14:26:54 +0200

    a

---
+++ Scharp/Vježbanjecs/Vjezbanje/Program.cs
@@ -0,0 +1,143 @@
+﻿/*double x;
+x = double.Parse(Console.ReadLine());
+Console.WriteLine((x>5.5) ? "Excellent" : null);*/
+/*float x;
+x = float.Parse(Console.ReadLine());
+Console.WriteLine((x>5.5) ? "Excellent" : "Not Excellent"); */
+
+/*int x;
+x = int.Parse(Console.ReadLine());
+x = x % 2;
+Console.WriteLine((x==0) ? "even" : "odd");*/
+
+/*var num1 = int.Parse(Console.ReadLine());
+var num2 = int.Parse(Console.ReadLine());
+Console.WriteLine((num1>num2) ? num1 : num2); */
+
+/*int x;
+x = int.Parse(Console.ReadLine());
+if (x==1)
+{
+    Console.WriteLine("Jedan");
+}
+if (x==2)
+{
+    Console.WriteLine("Dva");
+}
+if (x==3)
+{
+    Console.WriteLine("Tri");
+}
+if (x==4)
+{
+    Console.WriteLine("Četiri");
+}
+if (x==5)
+{
+    Console.WriteLine("Pet");
+}
+if (x==6)
+{
+    Console.WriteLine("Šest");
+}
+if (x==7)
+{
+    Console.WriteLine("Sedam");
+}
+if (x==8)
+{
+    Console.WriteLine("Osam");
+}
+if (x==9)
+{
+    Console.WriteLine("Devet");
+}
+else if (x>=10)
+{
+    Console.WriteLine("Number too big");
+};*/
+
+
+/* int number;
+double bonus=0;
+number = int.Parse(Console.ReadLine());
+if (number <= 100)
+{
+    bonus = 5;
+};
+ if (number > 100 && number <= 1000)
+{
+    bonus = 0.2 * number;
+}
+if (number > 1000)
+{
+    bonus = 0.1 * number;
+}
+if (number % 2==0)
+{
+    bonus += 1;
+}    
+if (number % 10==5)
+{
+    bonus += 2;
+}
+Console.WriteLine(bonus);
+Console.WriteLine(number + bonus); */
+
+/*string password;
+password = Console.ReadLine();
+Console.WriteLine((password== "sgcj3257!!%") ? "Dobro došli"
+    : "Kriva lozinka"); */
+
+int age;
+string gender;
+Console.WriteLine("Unesi godine: ");
+age = int.Parse(Console.ReadLine());
+Console.WriteLine("Unesi spol: ");
+gender = Console.ReadLine();
+
+if (age >= 16 && gender == "m")
+{
+    Console.WriteLine("Gospodin");
+}
+else if (age <= 16 && gender == "m")
+{
+    Console.WriteLine("Dječak");
+}
+else if (age >= 16 && gender == "z")
+{
+    Console.WriteLine("Gospođa");
+}
+else if (age <= 16 && gender == "z")
+{
+    Console.WriteLine("Gospođica");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
