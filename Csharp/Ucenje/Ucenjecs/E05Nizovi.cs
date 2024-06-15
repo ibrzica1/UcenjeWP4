1e3a72b940d26cf9f55567402439d5a9a240e43c
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-06-15 08:51:05 +0200

    E03Nizovi

---
+++ Csharp/Ucenje/Ucenjecs/E05Nizovi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenjecs
+{
+    internal class E05Nizovi
+    {
+        public static void Izvedi()
+        {
+            int[] godine = new int[12];
+            godine[0] = 43;
+            godine[godine.Length - 1] = 23;
+            Console.WriteLine(godine);
+            Console.WriteLine(string.Join(",", godine));
+
+            object[] niz = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
+            Console.WriteLine(niz[2]);
+
+            string[] gradovi = { "Osijek" };
+            Console.WriteLine(niz[0]);
+
+            int[,] tablica =
+            {
+                { 1, 2, 3
+                },
+                { 4, 5, 6
+                },
+                { 7, 8, 9
+                }
+            };
+            Console.WriteLine(tablica[0,2]);
+
+        }
+    }
+}
+    
+
--- Csharp/Ucenje/Ucenjecs/Program.cs
+++ Csharp/Ucenje/Ucenjecs/Program.cs
@@ -1,6 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+using Ucenjecs;
+E03Z2.Izvedi();
+//E05Nizovi.Izvedi();
+//E04Z1.Izvedi();
+//E04UvjetnoGrananjeSwitch.Izvedi();
+//E03Z1.Izvedi();
+//E03UvjetnoGrananjeIF.Izvedi();
+//E01Z3.Izvedi();
+//E01Z1.Izvedi();
+//E02VarijableTipovi_PodatakaOperatori.Izvedi();
+//E01UlazIzlaz.Izvedi();
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
 Console.WriteLine(7);
 Console.Write(true);
 Console.WriteLine("Igor");
+*/
