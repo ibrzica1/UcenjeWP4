bf0abf731deb9c2cf071a6aafdaf3077020d6496
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-07-11 11:21:53 +0200

    KonzolnaAplikacija_

---
+++ Csharp/Kuca/KucaVjezbanje/KonzolnaAplikacija/Model/Grupa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KucaVjezbanje.KonzolnaAplikacija.Model
+{
+    internal class Grupa: Entitet
+    {
+        public string? Naziv { get; set; }
+        public Smjer? Smjer { get; set; }
+        public string? Predavac { get; set; }
+        public int? MaksimalnoPolaznika { get; set; }
+        public List<Polaznik>? Polaznici { get; set; }
+
+
+
+    }
+}
---
+++ Csharp/Kuca/KucaVjezbanje/KonzolnaAplikacija/ObradaSmjer.cs
@@ -0,0 +1,45 @@
+﻿using KucaVjezbanje.KonzolnaAplikacija.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KucaVjezbanje.KonzolnaAplikacija
+{
+    internal class ObradaSmjer
+    {
+        public List<Smjer> Smjerovi { get; set; }
+
+        public ObradaSmjer() 
+        {
+          Smjerovi = new List<Smjer>();
+
+
+
+        }
+
+        public void PrikaziIzbornik()
+        {
+            Console.WriteLine("Izbornik za rad s smjerovima");
+            Console.WriteLine("1. Pregled svih smjerova");
+            Console.WriteLine("2. Unos novog smjera");
+            Console.WriteLine("3. Promjena podataka postojećeg smjera");
+            Console.WriteLine("4. Brisanje smjera");
+            Console.WriteLine("5. Povratak na glavni izbornik");
+            OdabirOpcijeIzbornika();
+
+        }
+
+        private void OdabirOpcijeIzbornika()
+        {
+            switch(Pomocno.UcitajRasponBroja("Odaberi stavku izbornika",1,5))
+            {
+                case 5:
+                    Console.Clear();
+                    break;
+
+            }
+        }
+    }
+}
