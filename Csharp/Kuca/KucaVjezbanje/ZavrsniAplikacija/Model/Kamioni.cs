2c25816e984bf6be3049b643a7813e3a3a242e0c
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-08-10 21:59:15 +0200

    Update Kamioni.cs

--- Csharp/Kuca/KucaVjezbanje/ZavrsniAplikacija/Model/Kamioni.cs
+++ Csharp/Kuca/KucaVjezbanje/ZavrsniAplikacija/Model/Kamioni.cs
@@ -11,6 +11,7 @@ namespace KucaVjezbanje.ZavrsniAplikacija.Model
         public int? Kamion_ID { get; set; }
         public string? Reg_Oznaka { get; set; }
         public string? Marka { get; set; }
+        public DateTime? Istek_Reg { get; set; }
         public int? Godina_Proizvodnje { get; set; }
         public double? Prosjecna_Potrosnja_Goriva { get; set; }
     }
