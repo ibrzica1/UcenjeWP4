1df36e06df0a3099a67e290cd76a75ddb2405334
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-08-15 18:37:21 +0200

    Update CiklicnaTablica.cs

--- Csharp/Kuca/KucaVjezbanje/CiklicnaTablica.cs
+++ Csharp/Kuca/KucaVjezbanje/CiklicnaTablica.cs
@@ -17,14 +17,65 @@ namespace KucaVjezbanje
             int stupac = int.Parse(Console.ReadLine());
             int redak = int.Parse(Console.ReadLine());
             int redovi = redak * redak;
-            int[] brojevi = new int[redovi];
             int[,] tablica = new int[stupac,redak];
+            int minRedak = 0;
             int maxRedak = redak - 1;
+            int minStupac = 0;
             int maxStupac = stupac - 1;
-            int niz = 0;
+            int broj = 1;
 
+            while (broj <= redovi)
+            {
 
-            for (int i = 0; i < stupac; i++)
+                for (int i = maxRedak; i >= minRedak; i--)
+                {
+                    if (tablica[maxStupac, i] == 0)
+                    {
+                        tablica[maxStupac, i] = broj++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                maxStupac--;
+
+                for (int i = maxStupac; i >= minStupac; i--)
+                {
+                    if (tablica[i, minRedak] == 0)
+                    {
+                        tablica[i, minRedak] = broj++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                minRedak++;
+
+                for (int i = minRedak; i <= maxRedak; i++)
+                {
+                    if (tablica[i, maxStupac] == 0)
+                    {
+                        tablica[i, maxStupac] = broj++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                maxStupac--;
+            }
+            for (int i = 0; i < redak; i++)
+            {
+                for (int j = 0; j < stupac; j++)
+                {
+                    Console.Write("\t" + tablica[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+            /*for (int i = 0; i < stupac; i++)
             {
 
                  Console.Write("\n");
@@ -33,7 +84,7 @@ namespace KucaVjezbanje
                     tablica[i,j] = niz+=1;
                     Console.Write(tablica[i,j]);
                 } 
-            }
+            }*/
 
         }
     }
