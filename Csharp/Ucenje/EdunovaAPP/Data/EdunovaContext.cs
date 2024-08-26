e097dfa4663f6bee9c4fd9e0c664ece4b899a035
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-08-26 17:59:48 +0200

    EdunovaAPP

---
+++ Csharp/Ucenje/EdunovaAPP/Data/EdunovaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EdunovaAPP.Data
+{
+    public class EdunovaContext:DbContext
+    {
+        public EdunovaContext(DbContextOptions<EdunovaContext> opcije): base(opcije)
+        {
+
+        }
+    }
+}
