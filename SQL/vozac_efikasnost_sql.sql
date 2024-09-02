8a1c513a19c78712e445d126de30a3bb4f0ba7d9
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-09-02 20:41:32 +0200

    Update vozac_efikasnost_sql.sql

--- SQL/vozac_efikasnost_sql.sql
+++ SQL/vozac_efikasnost_sql.sql
@@ -7,21 +7,23 @@ go
 
 use vozac_efikasnost;
 
-create table ture(
+create table tura(
 tura_id int not null primary key identity(1,1),
 relacija varchar(50) not null,
-datum datetime not null,
 udaljenost decimal(10,2) not null,
 prijedeni_km decimal (10,2)not null,
 potrosnja_goriva decimal (6,2) not null,
 kamion_id int not null,
-vozac_id int not null
+vozac_id int not null,
+datum_pocetak datetime not null,
+datum_zavrsetak datetime not null
 );
 
 create table kamioni(
 kamion_id int not null primary key identity(1,1),
 reg_oznaka varchar(15) not null,
 marka varchar(35) not null,
+istek_registracije datetime null,
 godina_proizvodnje int not null,
 prosjecna_potrosnja_goriva decimal (4,2) not null
 );
@@ -35,8 +37,8 @@ istek_ugovora datetime not null
 );
 
 
-alter table ture add foreign key (kamion_id) references kamioni(kamion_id);
-alter table ture add foreign key (vozac_id)  references vozaci(vozac_id);
+alter table tura add foreign key (kamion_id) references kamioni(kamion_id);
+alter table tura add foreign key (vozac_id)  references vozaci(vozac_id);
 
 
 insert into vozaci (ime,prezime,datum_rodenja,istek_ugovora) values 
@@ -49,27 +51,27 @@ insert into vozaci (ime,prezime,datum_rodenja,istek_ugovora) values
 );
 
 insert into kamioni (reg_oznaka,marka,godina_proizvodnje,
-prosjecna_potrosnja_goriva) values
-('OS2265PK','Scania R500',2019,23.56),
-('OS7754OT','Scania R500 V8',2010,30.11),
-('OS8657IH','Man TG 420',2010,29.13),
-('OS1145TU','Man TGX 460',2018,27.50),
-('OS3764RR','Volvo FH500',2015,25.88),
-('OS8847EH','Volvo FH460',2012,26.90
+prosjecna_potrosnja_goriva,istek_registracije) values
+('OS2265PK','Scania R500',2019,23.56,'2024-11-02'),
+('OS7754OT','Scania R500 V8',2010,30.11,'2025-05-22'),
+('OS8657IH','Man TG 420',2010,29.13,'2025-02-12'),
+('OS1145TU','Man TGX 460',2018,27.50,'2024-10-25'),
+('OS3764RR','Volvo FH500',2015,25.88,'2025-04-11'),
+('OS8847EH','Volvo FH460',2012,26.90,'2025-02-12'
 );
 
-insert into ture (relacija,datum,udaljenost,
+insert into tura (relacija,datum_pocetak,datum_zavrsetak,udaljenost,
 prijedeni_km,potrosnja_goriva,kamion_id,vozac_id) values
-('osijek-zagreb','2024-03-15',223.34,230.12,68.20,6,1),
-('osijek-split','2024-04-16',687.09,695.55,215.55,3,1),
-('osijek-čakovec','2024-03-17',242.45,244.34,70.22,2,2),
-('osijek-rijeka','2024-04-13',444.67,447.21,120.11,5,2),
-('osijek-zagreb','2024-03-14',223.34,240.25,65.58,1,3),
-('osijek-rijeka','2024-04-09',444.67,470.77,137.55,4,3),
-('osijek-sl.brod','2024-03-20',94.67,100.45,37.54,3,4),
-('osijek-čakovec','2024-04-09',242.45,257.45,77.23,5,4),
-('osijek-split','2024-03-13',687.09,689.22,197.34,2,5),
-('osijek-zagreb','2024-04-23',223.34,224.67,59.91,3,5),
-('osijek-sl.brod','2024-03-08',94.67,96.02,27.46,6,6),
-('osijek-split','2024-04-15',687.09,691.11,196.11,4,6
+('osijek-zagreb','2024-03-15','2024-03-15', 223.34,230.12,68.20,6,1),
+('osijek-split','2024-04-16','2024-04-17',687.09,695.55,215.55,3,1),
+('osijek-čakovec','2024-03-17','2024-03-17', 242.45,244.34,70.22,2,2),
+('osijek-rijeka','2024-04-13','2024-04-13',444.67,447.21,120.11,5,2),
+('osijek-zagreb','2024-03-14','2024-03-14',223.34,240.25,65.58,1,3),
+('osijek-rijeka','2024-04-09','2024-04-10',444.67,470.77,137.55,4,3),
+('osijek-sl.brod','2024-03-20','2024-03-20',94.67,100.45,37.54,3,4),
+('osijek-čakovec','2024-04-09','2024-04-09',242.45,257.45,77.23,5,4),
+('osijek-split','2024-03-13','2024-03-14',687.09,689.22,197.34,2,5),
+('osijek-zagreb','2024-04-23','2024-04-23',223.34,224.67,59.91,3,5),
+('osijek-sl.brod','2024-03-08','2024-03-08',94.67,96.02,27.46,6,6),
+('osijek-split','2024-04-15','2024-04-16',687.09,691.11,196.11,4,6
 );
