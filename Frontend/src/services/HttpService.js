2fa4e79e1eb6d6ab304c37cf05f2e2313cb9b3a5
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-09-13 19:56:50 +0200

    Create HttpService.js

---
+++ Frontend/src/services/HttpService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { BACKEND_URL } from "../constans";
+
+
+export const HttpService = axios.create({
+    baseURL: BACKEND_URL,
+    headers: {
+        'Content-Type':'aplication/json'
+    }
+});
